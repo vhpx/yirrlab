7ccd65f711f56b48fba42827d6470b6b1edb6832
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-12-28 12:06:19 +0700

    Fixed footer floating bug

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="absolute font-semibold bottom-0 bg-zinc-700 flex justify-between inset-x-0 text-white px-4 py-0.5">
+    <div className="fixed font-semibold bottom-0 bg-zinc-700 flex justify-between inset-x-0 text-white px-4 py-0.5">
       <div>Contact us</div>
       <div>© 2022 YirrLab</div>
     </div>
